52652049564c43aca1eae6e80947bf8e19f1f06b
william de freitas <williamfacme@gmail.com>
2022-11-26 15:30:18 -0300

    chore att

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,27 @@ import styles from './App.module.css';
 import './global.css';
 import { Sidebar } from "./components/Sidebar";
 
+const posts = [
+  {
+      id:1,
+      author:{          
+          avatarUrl:"https://avatars.githubusercontent.com/u/89050239?v=4",
+          name:"william de freitas",
+          role:"Analista programaor"
+      },
+      content: [
+        {type : 'paragraph', content: 'Fala galeraa 👋'},        
+        {type : 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return,'}, 
+        {type : 'paragraph', content: 'evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},    
+        {type : 'link', content: '👉 jane.design/doctorcare'},    
+        {type : 'link', content: ' #novoprojeto | '},
+        {type : 'link', content: ' #nlw | '},
+        {type : 'link', content: ' #rocketseat'}       
+      ],
+      publishetAt: new Date('2022/11/26')
+  },
+]
+
 export function App() {
   return (
     <>
@@ -13,8 +34,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar  />
         <main>
-          <Post />          
-          <Post />          
+          {posts.map(post => {
+            return (
+              <Post
+                key = {post.id}
+                author={post.author}
+                content={post.content}
+                publishetAt={post.publishetAt}
+              />
+            )
+          })}      
         </main>
       </div>   
     </>    
--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,37 +1,53 @@
+import {format, formatDistanceToNow} from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
+
+
 import { Avatar } from '../Avatar';
 import { Comment } from '../Comment';
+
 import styles from './styles.module.css';
 
-export function Post(){
+export function Post({author, content, publishetAt}){
+    const publishedDateFormatted = format(publishetAt, "d 'de' LLLL 'às' HH:mm'h'",{
+        locale:ptBr
+    });
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishetAt,{
+        locale:ptBr,
+        addSuffix:true
+    })
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
                     <Avatar
-                        avatar="https://avatars.githubusercontent.com/u/89050239?v=4"
+                        avatar={author.avatarUrl}
                     />
                     <div className={styles.authorInfo}>
-                        <strong>william</strong>
-                        <span>programador junior</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="10 de outubro de 2022" dateTime="2022-08-10">Publicado a 1 hora</time>
+                <time 
+                    title={publishedDateFormatted}
+                    dateTime={publishetAt.toISOString()}
+                >
+                    {publishedDateRelativeToNow}
+
+                </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>
-                    Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, 
-                    evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-                </p>
-                <p>👉 <a href="#">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="#">#novoprojeto | </a> 
-                    <a href="#">#nlw | </a> 
-                    <a href="#">#rocketseat</a>
-                </p>
-                
+                {
+                    content.map(item =>{
+                        if(item.type == 'paragraph')
+                            return <p>{item.content}</p>
+
+                        return <a href="#">{item.content}</a>
+                    })
+                }
             </div>
 
             <form className={styles.commentForm}>
